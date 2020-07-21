30b31ba6f43c962735f39cc71f9a0ab3c209701b
Rabz2210 <topspin2210@gmail.com>
2020-07-21 20:38:02 +0530

    initial commit

---
+++ inputprac.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    string str;
+    int t =4;
+    while(t--){
+        getline(cin,str);
+
+        while (str.length()==0)
+        {
+            getline(cin,str);
+        }
+        cout<<str<<":newline"<<"\n";
+    }
+    return 0;
+}
